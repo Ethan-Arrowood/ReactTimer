53de1cf732e106c9089c845a2117c8336e903c7f
ethan-arrowood <ethan.arrowood@gmail.com>
2016-05-30 21:28:06 -0400

    Updated foundation and karma

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -41,7 +41,7 @@ var Timer = React.createClass({
     var {count, timerStatus} = this.state;
     return (
       <div>
-        <h1 className="page-title">Timer</h1>
+        <h1 className="page-title">Timer App</h1>
         <Clock totalSeconds={count}/>
         <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange}/>
       </div>
